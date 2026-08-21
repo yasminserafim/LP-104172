6832ed3c47be294d65c9c303ca80ece5aeccca2b
yasminserafim <yasmin.celestino@ba.estudante.senai.br>
2026-08-21 16:44:53 -0300

    Add files via upload

---
+++ 6- antecessor_sucessor.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+print("= SOLICITANDO DADOS =")
+numero = int(input("Digite um número: "))
+antecessor = int(numero - 1)
+sucessor = int(numero + 1)
+
+print("\n= EXIBINDO DADOS =")
+print("Antecessor: ", antecessor)
+print("Sucessor: ", sucessor)
+
+
+
